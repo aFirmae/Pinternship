0b249c7359ea69bdf80c69f9841a35db657fa95e
afirmae <nilashis37@gmail.com>
2026-01-24 16:20:22 +0530

    Complete 1st TypeScript Case Study

---
+++ case studies/typescript/cs1.ts
@@ -0,0 +1,6 @@
+let message: string = "Greetings!";
+let myName: string = "Nilashis Saha";
+// let age: string = 21; // Type Error
+let age: number = 21;
+
+console.log(`${message} My name is ${myName} and I am ${age} years old.`);
